6f877c05214965d538f508f1fef7804003f909d7
blis <brclis@gmail.com>
2024-02-21 15:30:13 -0800

    Add files via upload
    First Test

---
+++ com.github.test/src/com/github/test/HelloJava.java
@@ -0,0 +1,11 @@
+package com.github.test;
+
+public class HelloJava {
+	
+	public static void main(String[] args) throws Exception {  
+		 
+		
+	     System.out.println("args.length: " + args.length);
+	}
+
+}
